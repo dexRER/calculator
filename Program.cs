edc0021b05cce0da45e5b474565ee9ae02ec0323
Shvets Andrey <ll1ontte@gmail.com>
2026-01-06 17:34:18 +0300

    #VERSION №2
    All fix:
    
    1. Fix letters in the words like "Sigh" > "Sign
    
    All updates:
    
    1. Add loop "for" into the program for infinity  duration of working
    2. Add option of choice in the first step in the program.

--- Program.cs
+++ Program.cs
@@ -1,28 +1,49 @@
-﻿float code1;
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+
+float code1;
 float code2;
 string? sign;
 
-for (Console.WriteLine("## ДОБРО ПОЖАЛОВАТЬ В КАЛЬКУЛЯТОР"); ;)
+Console.WriteLine("Hi! What do you want?");
+Console.Write("1. Calculator: ");
+int userChoice = Convert.ToInt32(Console.ReadLine());
 {
-
-    Console.Write("Enter your first number: ");
-    code1 = Convert.ToSingle(Console.ReadLine());
-    Console.Write("Enter a sign ['**' '+' '-' '/' '*']");
-    sign = Console.ReadLine();
-    Console.Write("Enter your second number: ");
-    code2 = Convert.ToSingle(Console.ReadLine());
-
-    float result = sign switch
+    if ( userChoice == 1)
     {
-        "+" => (float)code1 + (float)code2,
-        "-" => (float)code1 - (float)code2,
-        "*" => (float)code1 * (float)code2,
-        "/" => code2 != 0 ? (float)code1 / (float)code2 : throw new ArgumentException("Unknown Sigh!"),
-        "**" => (float)Math.Pow((float)code1, (float)code2),
-        _ => throw new ArgumentException("Unknown Sigh!")
-    };
+        Calculator();
+    }
+    else
+    {
+        Console.WriteLine("Sorry but not now");
+    }
+}
 
-    Console.WriteLine(result);
 
-    Thread.Sleep(1000);
+void Calculator()
+{
+    for (Console.WriteLine("## ДОБРО ПОЖАЛОВАТЬ В КАЛЬКУЛЯТОР"); ;)
+    {
+
+        Console.Write("Enter your first number: ");
+        code1 = Convert.ToSingle(Console.ReadLine());
+        Console.Write("Enter a sign ['**' '+' '-' '/' '*']");
+        sign = Console.ReadLine();
+        Console.Write("Enter your second number: ");
+        code2 = Convert.ToSingle(Console.ReadLine());
+
+        float result = sign switch
+        {
+            "+" => (float)code1 + (float)code2,
+            "-" => (float)code1 - (float)code2,
+            "*" => (float)code1 * (float)code2,
+            "/" => code2 != 0 ? (float)code1 / (float)code2 : throw new ArgumentException("Unknown Sign!"),
+            "**" => (float)Math.Pow((float)code1, (float)code2),
+            _ => throw new ArgumentException("Unknown Sign!")
+        };
+
+        Console.WriteLine(result);
+
+        Thread.Sleep(1000);
+    }
 }
--- obj/Debug/net10.0/digitCalculator.AssemblyInfo.cs
+++ obj/Debug/net10.0/digitCalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("digitCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4602af314d9576201ebef87bf834384506536be8")]
 [assembly: System.Reflection.AssemblyProductAttribute("digitCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("digitCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
