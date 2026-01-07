2df4bd7726bc9a6f63f2eca7e2ed05608d54c2e3
Shvets Andrey <ll1ontte@gmail.com>
2026-01-07 03:08:47 +0300

    #VERSION №3
    All fix:
    
    -
    
    All updates:
    
    1. Now you can close the program at will.
    2. The program displays a list of options.
    3. A more minimalistic division by 0 error
    4. The program takes into account more user mistakes and does not crash.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,10 @@
-﻿using System.Globalization;
-using System.Runtime.CompilerServices;
-
-float code1;
+﻿float code1;
 float code2;
 string? sign;
+bool continueCalculating  = true;
 
 Console.WriteLine("Hi! What do you want?");
+StartCycle:
 Console.Write("1. Calculator: ");
 int userChoice = Convert.ToInt32(Console.ReadLine());
 {
@@ -15,16 +14,19 @@ int userChoice = Convert.ToInt32(Console.ReadLine());
     }
     else
     {
-        Console.WriteLine("Sorry but not now");
+        Console.WriteLine("We are don't have something like that");
+        goto StartCycle;
     }
 }
 
 
 void Calculator()
 {
-    for (Console.WriteLine("## ДОБРО ПОЖАЛОВАТЬ В КАЛЬКУЛЯТОР"); ;)
+    while(continueCalculating == true)
     {
-
+        Console.WriteLine("## ДОБРО ПОЖАЛОВАТЬ В КАЛЬКУЛЯТОР");
+        Console.WriteLine("---------------------------------\n");
+        CalcAgain:
         Console.Write("Enter your first number: ");
         code1 = Convert.ToSingle(Console.ReadLine());
         Console.Write("Enter a sign ['**' '+' '-' '/' '*']");
@@ -32,12 +34,18 @@ void Calculator()
         Console.Write("Enter your second number: ");
         code2 = Convert.ToSingle(Console.ReadLine());
 
+        if(sign == "/" && code2 == 0)
+        {
+            Console.WriteLine("Really ?");
+            goto CalcAgain;
+        }
+
         float result = sign switch
         {
             "+" => (float)code1 + (float)code2,
             "-" => (float)code1 - (float)code2,
             "*" => (float)code1 * (float)code2,
-            "/" => code2 != 0 ? (float)code1 / (float)code2 : throw new ArgumentException("Unknown Sign!"),
+            "/" => (float)code1 / (float)code2,
             "**" => (float)Math.Pow((float)code1, (float)code2),
             _ => throw new ArgumentException("Unknown Sign!")
         };
@@ -45,5 +53,12 @@ void Calculator()
         Console.WriteLine(result);
 
         Thread.Sleep(1000);
+        Console.WriteLine("Write 'n' to close this app or write 'y' to continue");
+        if(Console.ReadLine() == "n")
+        {
+            continueCalculating = false;
+        }
+
+        Console.WriteLine();
     }
-}
+}   
